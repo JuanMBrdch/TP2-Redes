b7286cf8074dd94194afdb8396085ee7e451772e
SantiagoF03 <102191455+SantiagoF03@users.noreply.github.com>
2023-11-06 17:32:27 -0300

    a

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,16 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Bullet : NetworkBehaviour
 {
-    void Start()
+    public float speed;
+    public float timeToDestroy = 5;
+    Rigidbody _rb;
+    PlayerModel _ownerModel;
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+
+    }
+    //private void Update()
+    //{
+    //    if (!NetworkManager.Singleton.IsServer) return;
+
+    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!NetworkManager.Singleton.IsServer) return;
+        //if (!IsOwner) return;
+        var playerModel = other.GetComponent<PlayerModel>();
+        if (playerModel == _ownerModel) return;
+        if (playerModel != null)
+        {
+            playerModel.TakeDamage();
+        }
+
+        Destroy();
+
+    }
+    void Destroy()
+    {
+        var netObj = GetComponent<NetworkObject>();
+        netObj.Despawn(true);
+        Destroy(gameObject);
+    }
+    public void Shoot(PlayerModel ownerModel, Vector3 dir)
+    {
+        _ownerModel = ownerModel;
+        _rb.velocity = dir * speed;
+        StartCoroutine(WaitToDestroy());
+    }
+    IEnumerator WaitToDestroy()
+    {
+        yield return new WaitForSeconds(timeToDestroy);
+        Destroy();
     }
 
-    void Update()
-    {
-        
-    }
 }
